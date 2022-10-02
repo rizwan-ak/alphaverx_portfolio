495a8072a198444df776bcb51fc09c95ab625ea5
ibrahimnabi <ibrahimnabi80@gmail.com>
2022-10-02 23:45:32 +0500

    updated website

--- src/blocks/team/data.jsx
+++ src/blocks/team/data.jsx
@@ -1,95 +1,84 @@
-import React from 'react';
-import { FaLinkedinIn } from 'react-icons/fa';
+import React from "react";
+import { FaLinkedinIn } from "react-icons/fa";
 
 let data = [
-    {
-        images: require('../../assets/team/rizwan.jpg'),
-        title: 'Rizwan Ahmad',
-        designation: 'Co-Founder',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/rizwan-ahmed-7242631b6/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/builder.jpg'),
-        title: 'Ibrahim Nabi',
-        designation: 'Co-Founder',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/ibrahim-nabi-2b74091b2/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/mani.jpg'),
-        title: 'Abdul Manan',
-        designation: 'Sr. Full Stack Engineer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/abdul-manan-qureshi-72886812a/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/bukhari.jpg'),
-        title: 'Ali Zain',
-        designation: 'Full Stack Engineer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/ali-zain-bukhari-071368154/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/ammar.png'),
-        title: 'Ammar Ali',
-        designation: 'Sr. Game Developer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/ammar-ali-59832a1b7/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/rana.jpg'),
-        title: 'Mohsin Rana',
-        designation: 'Full Stack Engineer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/mohisn-rana-30b903187/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/haris.jpg'),
-        title: 'Harris Memoon',
-        designation: 'Front-end Developer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://www.linkedin.com/in/haris-memoon-4aa179194/',
-            },
-        ],
-    },
-    {
-        images: require('../../assets/team/wajid.jpg'),
-        title: 'Wajid Javaid',
-        designation: 'Full Stack Developer',
-        socialNetwork: [
-            {
-                icon: <FaLinkedinIn />,
-                url: 'https://pk.linkedin.com/public-profile/in/wajid-javed-4b86a2134',
-            },
-        ],
-    },
+  {
+    images: require("../../assets/team/rizwan.jpg"),
+    title: "Rizwan Ahmad",
+    designation: "Founder",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/rizwan-ahmed-7242631b6/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/mani.jpg"),
+    title: "Abdul Manan",
+    designation: "Software Engineer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/abdul-manan-qureshi-72886812a/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/bukhari.jpg"),
+    title: "Ali Zain",
+    designation: "Full Stack Engineer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/ali-zain-bukhari-071368154/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/ammar.png"),
+    title: "Ammar Ali",
+    designation: "React Developer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/ammar-ali-59832a1b7/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/rana.jpg"),
+    title: "Mohsin Rana",
+    designation: "Full Stack Engineer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/mohisn-rana-30b903187/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/haris.jpg"),
+    title: "Harris Memoon",
+    designation: "Front-end Developer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/haris-memoon-4aa179194/",
+      },
+    ],
+  },
+  {
+    images: require("../../assets/team/wajid.jpg"),
+    title: "Wajid Javaid",
+    designation: "Full Stack Developer",
+    socialNetwork: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://pk.linkedin.com/public-profile/in/wajid-javed-4b86a2134",
+      },
+    ],
+  },
 ];
 
 export default data;
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import { FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
+import React, { Component } from "react";
+import { FaWhatsapp } from "react-icons/fa";
 
 const SocialShare = [
-    { Social: <HiOutlineMail />, link: 'https://www.facebook.com/' },
-    { Social: <FaWhatsapp />, link: 'https://wa.me/+923234276408/?text=Hey Alphaverx, I need ... ' },
-    { Social: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/rizwan-ahmed-7242631b6/' },
+  {
+    Social: <FaWhatsapp />,
+    link: "https://wa.me/+923044145274/?text=Hey Alphaverx, I need ... ",
+  },
 ];
 class Footer extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <footer className="footer-area">
-                    <div className="footer-wrapper">
-                        <div className="row align-items-end row--0">
-                            <div className="col-lg-6">
-                                <div className="footer-left">
-                                    <div className="inner">
-                                        <span>Ready To order?</span>
-                                        <h2 className="mt-3">
-                                            Let's get <br /> to work
-                                        </h2>
-                                        {/* <a className="rn-button-style--2" href="/contact">
+  render() {
+    return (
+      <React.Fragment>
+        <footer className="footer-area">
+          <div className="footer-wrapper">
+            <div className="row align-items-end row--0">
+              <div className="col-lg-6">
+                <div className="footer-left">
+                  <div className="inner">
+                    <span>Ready To order?</span>
+                    <h2 className="mt-3">
+                      Let's get <br /> to work
+                    </h2>
+                    {/* <a className="rn-button-style--2" href="/contact">
                                             <span>Contact Us</span>
                                         </a> */}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6">
-                                <div className="footer-right" data-black-overlay="6">
-                                    <div className="row">
-                                        {/* <div className="col-lg-6 col-sm-6 col-12">
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="footer-right" data-black-overlay="6">
+                  <div className="row">
+                    {/* <div className="col-lg-6 col-sm-6 col-12">
                                             <div className="footer-link">
                                                 <h4>Quick Link</h4>
                                                 <ul className="ft-link">
@@ -40,44 +40,55 @@ class Footer extends Component {
                                                 </ul>
                                             </div>
                                         </div> */}
-                                        <div className="col-lg-6 col-sm-6 col-12 mt_mobile--30">
-                                            <div className="footer-link">
-                                                <h4>Say Hello</h4>
-                                                <ul className="ft-link">
-                                                    {/* <li>
+                    <div className="col-lg-6 col-sm-6 col-12 mt_mobile--30">
+                      <div className="footer-link">
+                        <h4>Say Hello</h4>
+                        <ul className="ft-link">
+                          {/* <li>
                                                         <a href="mailto:admin@example.com">sales@alphaverx.com</a>
                                                     </li> */}
-                                                    <li>
-                                                        <a href="mailto:hr@example.com">alphaverx@gmail.com</a>
-                                                    </li>
-                                                </ul>
+                          <li>
+                            <a
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href="mailto:support@alphaverx.com"
+                            >
+                              support@alphaverx.com
+                            </a>
+                          </li>
+                        </ul>
 
-                                                <div className="social-share-inner">
-                                                    <ul className="social-share social-style--2 d-flex justify-content-start liststyle mt--15">
-                                                        {SocialShare.map((val, i) => (
-                                                            <li key={i}>
-                                                                <a href={`${val.link}`}>{val.Social}</a>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* End Single Widget  */}
-
-                                        <div className="col-lg-12">
-                                            <div className="copyright-text">
-                                                <p>Copyright © 2020 Alphaverx. All Rights Reserved.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="social-share-inner">
+                          <ul className="social-share social-style--2 d-flex justify-content-start liststyle mt--15">
+                            {SocialShare.map((val, i) => (
+                              <li key={i}>
+                                <a
+                                  rel="noopener noreferrer"
+                                  href={`${val.link}`}
+                                >
+                                  {val.Social}
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
                         </div>
+                      </div>
                     </div>
-                </footer>
-            </React.Fragment>
-        );
-    }
+                    {/* End Single Widget  */}
+
+                    <div className="col-lg-12">
+                      <div className="copyright-text">
+                        <p>Copyright © 2020 Alphaverx. All Rights Reserved.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 export default Footer;
